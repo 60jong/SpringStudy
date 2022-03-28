89222748edfce21591e1a8572a60f9aa2a95741d
KyeonJong-Yoo <rudwhd515@gmail.com>
2022-03-28 20:15:17 +0900

    JPA 사용

---
+++ spring_aws/sourceCode/project/ykj-springboot-webservice/src/test/java/com/ykj/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.ykj.springboot.web.dto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        String name = "YKJ";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
