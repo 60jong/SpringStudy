9981cd464c8cecaa7574185dcf8fc59db91e3a0d
KyeonJong-Yoo <rudwhd515@gmail.com>
2022-04-05 14:59:10 +0900

    springboot GetMapping, PostMapping, PutMapping, DeleteMapping을 메모리 상의 데이터를 이용해 다뤄봄

--- springweb/src/main/java/com/jongky/prac/springweb/controller/UserProfileController.java
+++ springweb/src/main/java/com/jongky/prac/springweb/controller/UserProfileController.java
@@ -1,9 +1,7 @@
 package com.jongky.prac.springweb.controller;
 
 import com.jongky.prac.springweb.model.UserProfile;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -18,8 +16,14 @@ public class UserProfileController {
     @PostConstruct
     public void init() {
         userMap = new HashMap<String, UserProfile>();
-        userMap.put("1", new UserProfile("1", "유경종", "4903-9081", "서울시"));
-        userMap.put("2", new UserProfile("2", "주은진", "2715-0288", "수원시"));
+        userMap.put("1", new UserProfile("1", "ykj", "4903-9081", "Seoul"));
+        userMap.put("2", new UserProfile("2", "jej", "2715-0288", "Suwon"));
+        userMap.put("3", new UserProfile("3", "kmc", "2715-0288", "Seoul"));
+        userMap.put("4", new UserProfile("4", "iyh", "2715-0288", "Seoul"));
+        userMap.put("5", new UserProfile("5", "kjw", "2715-0288", "Seoul"));
+        userMap.put("6", new UserProfile("6", "kth", "2715-0288", "Suwon"));
+        userMap.put("7", new UserProfile("7", "syd", "2715-0288", "Gangleung"));
+        userMap.put("8", new UserProfile("8", "lhk", "2715-0288", "Seoul"));
     }
 
     @GetMapping("/user/{id}")
@@ -30,5 +34,24 @@ public class UserProfileController {
     @GetMapping("/user/all")
     public List<UserProfile> getUserProfileList() {
         return new ArrayList<UserProfile>(userMap.values());
+    }
+
+    @PostMapping("/user/{id}")
+    public void postUserProfile(@PathVariable(name = "id") String id, String name, String phone, String address) {
+        UserProfile user = new UserProfile(id, name, phone, address);
+        userMap.put(id, user);
+    }
+
+    @PutMapping("/user/{id}")
+    public void putUserProfile(@PathVariable(name = "id") String id, String name, String phone, String address) {
+        UserProfile user = userMap.get(id);
+        user.setName(name);
+        user.setPhone(phone);
+        user.setAddress(address);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public void deleteUserProfile(@PathVariable(name = "id") String id) {
+        userMap.remove(id);
     }
 }
